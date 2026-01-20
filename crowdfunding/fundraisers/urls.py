8575125a7283a676199018abbc7493169c3a4b95
Kate Hoeffler <kate.hoeffler@team.telstra.com>
2026-01-20 20:25:27 +1100

    Created fundraiser and pledges

--- crowdfunding/fundraisers/urls.py
+++ crowdfunding/fundraisers/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fundraisers/', views.FundraiserList.as_view())
+    path('fundraisers/', views.FundraiserList.as_view()),
+    path('fundraisers/<int:pk>/', views.FundraiserDetail.as_view()),
+    path('pledges/',views.PledgeList.as_view())
 ]
--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Fundraiser
-from .serializers import FundraiserSerializer
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from .models import Fundraiser, Pledge
+from .serializers import FundraiserSerializer, PledgeSerializer
 
 class FundraiserList(APIView):
 
@@ -12,3 +14,48 @@ class FundraiserList(APIView):
         fundraisers = Fundraiser.objects.all()
         serializer = FundraiserSerializer(fundraisers, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+# this function will get called to check the function is valid before saving it to our database.
+        serializer = FundraiserSerializer(data=request.data)
+# .data is passing through all the serialized data - this line deserializes/decodes the data
+        if serializer.is_valid():
+# is_valid is a built in function - it checks if the data is valid then save to database.
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTPP_400_BAD_REQUEST
+        )
+
+class FundraiserDetail(APIView):
+# Fundraiser detail, I want the option to show more detailed information.
+    def get(self, request, pk):
+        fundraiser = get_object_or_404(Fundraiser, pk=pk)
+        # pk = primary key
+        serializer = FundraiserSerializer(fundraiser)
+        return Response(serializer.data)
+
+class PledgeList(APIView):
+
+    def get(self, request):
+        pledges = Pledge.objects.all()
+        serializer = PledgeSerializer(pledges, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+
+        serializer = PledgeSerializer(data=request.data)
+        if serializer.is_valid():
+           serializer.save()
+           return Response(
+               serializer.data,
+               status=status.HTTP_201_CREATED
+           )
+        return Response(
+           serializer.errors,
+           status=status.HTTP_400_BAD_REQUEST
+       )
